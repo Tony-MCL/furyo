8ff7d84c6b88df50d56985aafef74136485ff5f7
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-08-05 06:34:04 +0200

    Hide Android system navigation bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,30 @@
+import { useEffect } from "react";
+import { AppState, Platform } from "react-native";
+import * as NavigationBar from "expo-navigation-bar";
 import { Slot } from "expo-router";
 
 export default function Layout() {
+  useEffect(() => {
+    if (Platform.OS !== "android") {
+      return;
+    }
+
+    const hideNavigationBar = () => {
+      NavigationBar.setHidden(true);
+    };
+
+    hideNavigationBar();
+
+    const subscription = AppState.addEventListener("change", (state) => {
+      if (state === "active") {
+        hideNavigationBar();
+      }
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
   return <Slot />;
 }
