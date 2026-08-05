7958464efb05d1aad151b5498a46492d25c74de0
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-08-05 09:06:56 +0200

    Use PNG artwork on native builds

--- components/fury-artwork.tsx
+++ components/fury-artwork.tsx
@@ -6,7 +6,6 @@ import {
   View,
   type ViewStyle,
 } from "react-native";
-import { SvgUri } from "react-native-svg";
 
 type FuryArtworkKind = "logo" | "game-over";
 
@@ -21,35 +20,21 @@ const WEB_URIS: Record<FuryArtworkKind, string> = {
 };
 
 const NATIVE_ASSETS: Record<FuryArtworkKind, number> = {
-  logo: require("../public/fury-logo.svg"),
-  "game-over": require("../public/fury-game-over.svg"),
+  logo: require("../public/fury-logo.png"),
+  "game-over": require("../public/fury-game-over.png"),
 };
 
 export default function FuryArtwork({ kind, style }: FuryArtworkProps) {
-  if (Platform.OS === "web") {
-    return (
-      <View style={style}>
-        <Image
-          source={{ uri: WEB_URIS[kind] }}
-          resizeMode="contain"
-          style={{ width: "100%", height: "100%" }}
-        />
-      </View>
-    );
-  }
-
-  const resolvedAsset = Image.resolveAssetSource(NATIVE_ASSETS[kind]);
-
-  if (!resolvedAsset?.uri) {
-    return <View style={style} />;
-  }
-
   return (
     <View style={style}>
-      <SvgUri
-        width="100%"
-        height="100%"
-        uri={resolvedAsset.uri}
+      <Image
+        source={
+          Platform.OS === "web"
+            ? { uri: WEB_URIS[kind] }
+            : NATIVE_ASSETS[kind]
+        }
+        resizeMode="contain"
+        style={{ width: "100%", height: "100%" }}
       />
     </View>
   );
