82cee310c4213cea1c1e6e94ff52dbdbffb02714
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-08-08 12:05:10 +0200

    Remove runtime navigation bar controller from app startup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,5 @@
 import { Slot } from "expo-router";
-import NavigationBarController from "../components/navigation-bar-controller";
 
 export default function Layout() {
-  return (
-    <>
-      <NavigationBarController />
-      <Slot />
-    </>
-  );
+  return <Slot />;
 }
